152b28b2538f39111418906b08cf936badf71e85
Teo Roca <teo.roca798@gmail.com>
2021-12-12 11:59:58 +0100

    añade verify order deleteRemote

--- src/test/java/com/practica/integracion/TestValidUser.java
+++ src/test/java/com/practica/integracion/TestValidUser.java
@@ -155,6 +155,10 @@ public class TestValidUser {
 			} catch (SystemManagerException e) {
 				fail("Unexpected exception was thrown");
 			}
+			finally{
+				ordered.verify(authDAO).getAuthData(validUser.getId());
+				ordered.verify(genericDAO).deleteSomeData(validUser, remoteId);
+			}
 		}
 		@DisplayName("Should not delete remote")
 		@Test
@@ -170,6 +174,10 @@ public class TestValidUser {
 
 			} catch (SystemManagerException e) {
 				assertNotNull(e);
+			}
+			finally{
+				ordered.verify(authDAO).getAuthData(validUser.getId());
+				ordered.verify(genericDAO).deleteSomeData(validUser, remoteId);
 			}
 		}
 	}
